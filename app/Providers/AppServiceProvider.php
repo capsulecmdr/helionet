f4b18578fe47e663fc528e8a4e6f9b9a046db0a8
Orsiki <jwlittle13@gmail.com>
2025-12-06 09:53:53 -0700

    minor styling and namespace fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Horizon\Horizon;
+use app\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
